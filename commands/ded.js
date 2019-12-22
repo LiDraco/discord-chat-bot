ba8b45e7e9af57b2582553429782d9e0559106e2
Draco Li <draco.li1337@gmail.com>
2019-12-21 23:30:46 -0800

    Updated emoji command

--- commands/ded.js
+++ commands/ded.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ded',
     description: 'ded',
     execute(message) {
-        message.channel.send(`:gg1::gg2:`);
+        message.channel.send(`<:gg1:657824176354230272><:gg2:657824203654823958>`);
     },
 };
