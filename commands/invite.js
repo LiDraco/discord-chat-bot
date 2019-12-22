702ed12a9869e34fbb3fb31e51a043d2932226bb
Draco Li <draco.li1337@gmail.com>
2019-12-21 23:51:53 -0800

    Added invite command

--- commands/invite.js
+++ commands/invite.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'invite',
     description: 'invite',
     execute(message) {
-        message.channel.send(`Here is the invite link for the server https://discord.gg/zM79Jss`);
+        message.channel.send(`Here is the invite link for the server https://discord.gg/9D3PjqC`);
     },
 };
