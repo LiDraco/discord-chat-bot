40a3e02201265348d514644b5d41f98443264a03
Draco <draco@lawn-128-61-47-45.lawn.gatech.edu>
2022-02-14 00:00:26 -0500

    changed pat, updated help

--- commands/pat.js
+++ commands/pat.js
@@ -2,9 +2,6 @@ module.exports = {
     name: 'pat',
     description: 'pat!',
     execute(message) {
-        const pat = [
-            'https://i.imgur.com/MB09aRS.jpg'
-        ];
-        message.channel.send(pat);
+        cmessage.channel.send({ files: ["./media/pat.gif"] });
     },
 };
