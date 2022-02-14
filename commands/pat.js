492efeedaca3c8d077a21d07de4914b67f529cdd
Draco <draco@lawn-128-61-47-45.lawn.gatech.edu>
2022-02-14 00:02:21 -0500

    Typo fix

--- commands/pat.js
+++ commands/pat.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'pat',
     description: 'pat!',
     execute(message) {
-        cmessage.channel.send({ files: ["./media/pat.gif"] });
+        message.channel.send({ files: ["./media/pat.gif"] });
     },
 };
