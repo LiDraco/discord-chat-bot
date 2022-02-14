bf5bb7db076f82fb5d37c359c6966412d9a2618a
Draco <draco.li1337@gmail.com>
2022-02-14 01:12:13 -0500

    Added emoji error handling

--- commands/emoji.js
+++ commands/emoji.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'emoji!',
     execute(message) {
         const emojiList = message.guild.emojis.map(e => e.toString()).join(" ");
+        console.log(emojiList);
         if(emojiList.length<2000){
             message.channel.send(emojiList);
         }
